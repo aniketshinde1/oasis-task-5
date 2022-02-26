bf4542470c6928be4471acdcb42c3f428d72e0b4
aniket shinde <98145403+aniketshinde1@users.noreply.github.com>
2022-02-26 23:29:01 +0530

    Add files via upload

---
+++ c/simpleIntrest.cpp
@@ -0,0 +1,15 @@
+#include <stdio.h>
+int main (){
+	
+	int principle,rate,years;
+	 printf("the value of principle:\n");
+	 scanf("%d",&principle);
+	 printf("the value of rate:\n");
+	 scanf("%d",rate);
+	 printf("the value of years:\n");
+	 scanf("%d",&years);
+	 
+int SimpleIntrest=principle*rate*years;
+	 printf("The value of SimpleIntrest%d",SimpleIntrest);
+	 
+}
